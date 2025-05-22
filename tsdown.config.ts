cd236ce9f6f062767af52c3d50cfc8ce32beeeea
userquin <userquin@gmail.com>
2025-05-22 14:24:47 +0200

    fix(iife): export `SvgPacker.SvgPacker` instead `SvgPacker`

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,12 @@
+import * as fsp from 'node:fs/promises'
 import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { build } from 'esbuild'
 import stdLibBrowser from 'node-stdlib-browser'
 import plugin from 'node-stdlib-browser/helpers/esbuild/plugin'
 import { defineConfig } from 'tsdown/config'
+
+const root = fileURLToPath(new URL('.', import.meta.url))
 
 export default defineConfig({
   entry: 'src/index.ts',
@@ -19,12 +23,12 @@ export default defineConfig({
   hooks: {
     'build:done': async function () {
       await build({
-        entryPoints: [path.resolve('dist/index.mjs')],
-        outfile: path.resolve('dist/index.browser.js'),
+        entryPoints: [path.resolve(root, './dist/index.mjs')],
+        outfile: path.resolve(root, './dist/index.browser.js'),
         bundle: true,
         format: 'iife',
-        // dont't add globalName, it won't work since it will be exposed as SvgPacker.SvgPacker instead of SvgPacker
-        // globalName: 'SvgPacker',
+        // beware of the globalName, it won't work since it will be exposed as SvgPacker.SvgPacker instead of SvgPacker
+        globalName: 'SvgPacker',
         inject: [path.resolve('node_modules/node-stdlib-browser/helpers/esbuild/shim')],
         define: {
           global: 'global',
@@ -33,6 +37,19 @@ export default defineConfig({
         },
         plugins: [plugin(stdLibBrowser)],
       })
+      // will require the user to use SvgPacker.SvgPacker({...}) instead SvgPacker({...})
+      // this code will patch the default export
+      await editFile(path.resolve(root, './dist/index.browser.js'), (content) => {
+        return content.replace(
+          'return __toCommonJS(index_exports);',
+          'return __toCommonJS(index_exports).SvgPacker;',
+        )
+      })
     },
   },
 })
+
+async function editFile(filePath: string, edit: (content: string) => string) {
+  const content = await fsp.readFile(filePath, 'utf8')
+  await fsp.writeFile(filePath, edit(content), 'utf8')
+}
