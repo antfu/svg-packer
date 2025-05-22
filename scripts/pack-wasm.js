dda5fbaf07652c38e8772624a4d082ba868dda2d
userquin <userquin@gmail.com>
2025-05-22 13:22:09 +0200

    chore: resolve root folder via import.meta.url

--- scripts/pack-wasm.js
+++ scripts/pack-wasm.js
@@ -1,9 +1,13 @@
 import * as Buffer from 'node:buffer'
 import * as fsp from 'node:fs/promises'
+import { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const root = fileURLToPath(new URL('..', import.meta.url))
 
 packWasm()
 
 async function packWasm() {
-  const file = await fsp.readFile('./node_modules/ttf2woff2/jssrc/ttf2woff2.wasm')
-  await fsp.writeFile('./src/ttf2woff2.wasm.js', `export const wasmUrl = 'data:application/wasm;base64,${Buffer.Buffer.from(file).toString('base64')}';`, 'utf8')
+  const file = await fsp.readFile(resolve(root, `./node_modules/ttf2woff2/jssrc/ttf2woff2.wasm`))
+  await fsp.writeFile(resolve(root, './src/ttf2woff2.wasm.js'), `export const wasmUrl = 'data:application/wasm;base64,${Buffer.Buffer.from(file).toString('base64')}';`, 'utf8')
 }
