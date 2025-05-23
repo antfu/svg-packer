b4182b1f0ea62c39ffaed721c3252bccd314424a
userquin <userquin@gmail.com>
2025-05-23 17:58:15 +0200

    chore: add iife test at vite test

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,8 +1,16 @@
+import { resolve } from 'node:path'
 import { isCI } from 'std-env'
 import { SvgPackerVitePlugin } from 'svg-packer/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  server: {
+    fs: {
+      allow: [
+        resolve(__dirname, '..'),
+      ],
+    },
+  },
   plugins: [SvgPackerVitePlugin()],
   test: {
     browser: {
